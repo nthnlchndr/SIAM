7f8fe47db6c0003fd955babc7c85b4d74b32fe0d
Nathanael Chandra <nthnlchndr@student.ub.ac.id>
2022-09-30 13:58:35 +0700

    https://github.com/nthnlchndr/SIAM.git

---
+++ HOME/app.js
@@ -0,0 +1,30 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("regis-mode");
+  });
+});
+
+function moveSlider() {
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullets) => {
+  bullets.addEventListener("click", moveSlider);
+});
